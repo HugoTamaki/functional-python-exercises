ae48f206a63f8728bb2ed11377556e9dce6c9b9d
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-11-23 21:56:36 -0200

    Adding average height test

--- combination.py
+++ combination.py
@@ -1,6 +1,19 @@
 from functools import reduce
 from person import Person
 
+def average_height(people):
+  height_total = 0
+  height_count = 0
+  for person in people:
+    if 'height' in person:
+      height_total += person['height']
+      height_count += 1
+
+  if height_count > 0:
+    average_height = height_total / height_count
+
+  return average_height
+
 def total_letters(arr):
   letters_num = []
 
