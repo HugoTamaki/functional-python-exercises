02169e4b8437423b70293030937e72bd3041e573
Hugo Tamaki <hugokt2005@yahoo.com.br>
2017-11-29 18:31:02 -0200

    Adding decorators

--- exercise2.py
+++ exercise2.py
@@ -1,14 +1,2 @@
 
-bands = [{'name': 'Hell Riders', 'country': 'UK', 'active': False},
-         {'name': 'Dragons Crown', 'country': 'Germany', 'active': False},
-         {'name': 'Crystal Illusion', 'country': 'Spain', 'active': True}]
-
-def format_bands(bands):
-  for band in bands:
-    band['country'] = 'Canada'
-    band['active'] = True
-    band['name_length'] = len(band['name'])
-
-format_bands(bands)
-
-print bands
+# como seria sua implementação de map?
